86e897a65d70431641c47cb2263a1238dbd1214b
Randy Champagne <r-champagne@neiu.edu>
2015-09-21 18:32:33 -0400

    tutorial completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,63 +6,73 @@ $(function () {
 
 	$('.accordion').accordion();
 
+/*
+	// This code will activate modal on submit w/o validation
+	$('.submit').click(function(){
+		$('.ui.modal').modal('show');
+		// debugger;
+	});
+*/
 
-var validationObj = {
-	firstName: {
-		identifier  : 'first-name',
-		rules: [
-		{
-			type   : 'empty',
-			prompt : 'Please enter your first name'
-		}
-		]
-	},
-	lastName: {
-		identifier  : 'last-name',
-		rules: [
-		{
-			type   : 'empty',
-			prompt : 'Please enter your last name'
-		}
-		]
-	},
-	username: {
-		identifier : 'username',
-		rules: [
-		{
-			type   : 'empty',
-			prompt : 'Please enter a username'
-		}
-		]
-	},
-	password: {
-		identifier : 'password',
-		rules: [
-		{
-			type   : 'empty',
-			prompt : 'Please enter a password'
-		},
-		{
-			type   : 'length[6]',
-			prompt : 'Your password must be at least 6 characters'
-		},
-		{
-			type: 'containsNumbers',
-			prompt : 'Need a number'
-		}
-		]
-	}
-};
 
+	var validationObj = {
+		firstName: {
+			identifier  : 'first-name',
+			rules: [
+			{
+				type   : 'empty',
+				prompt : 'Please enter your first name'
+			}
+			]
+		},
+		lastName: {
+			identifier  : 'last-name',
+			rules: [
+			{
+				type   : 'empty',
+				prompt : 'Please enter your last name'
+			}
+			]
+		},
+		username: {
+			identifier : 'username',
+			rules: [
+			{
+				type   : 'empty',
+				prompt : 'Please enter a username'
+			}
+			]
+		},
+		password: {
+			identifier : 'password',
+			rules: [
+			{
+				type   : 'empty',
+				prompt : 'Please enter a password'
+			},
+			{
+				type   : 'length[6]',
+				prompt : 'Your password must be at least 6 characters'
+			},
+			{
+				type: 'containsNumbers',
+				prompt : 'Need a number'
+			}
+			]
+		}
+	};
+
+	// http://semantic-ui.com/behaviors/form.html for details
 	$.fn.form.settings.rules.containsNumbers = function(value){
 		var regex = new RegExp("[0-9]");
 		return regex.test(value);
 	}
 
-$('.form').form(validationObj, {
-	inline: false,
-	onSuccess: function(){
-		$('.ui.modal').modal('show');
-	} 
-});
+	$('.form').form(validationObj, {
+		inline: false,	// this allows you to add the validation inline
+		onSuccess: function(){
+			$('.ui.modal').modal('show');
+		} 
+	});
+
 });
