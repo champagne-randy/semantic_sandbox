b98cb751f7ca0fe0ce4250738527560438edd325
Randy Champagne <r-champagne@neiu.edu>
2015-09-21 19:55:21 -0400

    completed organization of public and src directories

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ var gulp = require('gulp'),
 	sass = require('gulp-ruby-sass');
 
 
+/*
+	ToDo:
+	- create a build-semantic task
+		copy all fonts
+		xxx minify and copy semantic specific files
+		simply copy content from ./src/libraries/semantic/packaged/ to ./public/libs/semantic/
+*/
 gulp.task('process-styles', function() {
 	return gulp.src('main.scss')
 });
